b6d3419ce2f7d8b72ea2e475450ea6800012ebb3
Alyson May <amay1421@yahoo.com>
2021-03-10 14:07:37 -0600

    aly's components

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./nav.css";
+
+function Nav (){
+    return (
+        <div class="holy-grail-grid">
+        <div class="holy-grail-header">
+          <h3>Header</h3>
+        </div>
+        </div>
+    )
+}
+
+export default Nav;
