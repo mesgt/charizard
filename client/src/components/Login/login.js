5979b06d46b4da640acc5c2bab5fe0a2cd485aae
JMan <justinvang07@gmail.com>
2021-03-11 10:33:21 -0600

    created login logic and css

---
+++ client/src/components/Login/login.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./login.css";
+
+function Login (){
+    return (
+        <div class="log-in-form">
+            <div class="text-center">
+                <h4>Log in to Everything</h4>
+                <input type="email" placeholder="somebody@example.com" />
+                <input type="password" placeholder="Password" />
+            <p><input type="submit" class="button expanded" value="Log in"></input></p>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
