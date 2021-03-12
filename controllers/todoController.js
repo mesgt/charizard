e2789b19a96d185bffeecf78231e1d706fd361d7
Maria Sargent <maria.marshall.83@gmail.com>
2021-03-12 10:33:20 -0600

    Removed scripts added as part of Heroku deployment troubleshooting.

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -2,36 +2,35 @@ const db = require("../models");
 
 // Defining methods for todoController
 module.exports = {
-    findAll: function (req, res) {
-        db.ToDo
-            .find(req.query)
-            .sort({ date: -1 })
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err));
-    },
+    // findAll: function (req, res) {
+    //     db
+    //         .findAll() //finding all users
+    //         .then(dbModel => res.json(dbModel))
+    //         .catch(err => res.status(422).json(err));
+    // },
     findById: function (req, res) {
-        db.ToDo
+        db
             .findById(req.params.id)
-            .then(dbModel => res.json(dbModel))
+            .then(dbModel => res.json(dbModel.todo))
             .catch(err => res.status(422).json(err));
     },
-    create: function (req, res) {
-        db.ToDo
-            .create(req.body)
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err));
-    },
+    // create: function (req, res) {
+    //     db
+    //         .create(req.body)
+    //         .then(dbModel => res.json(dbModel))
+    //         .catch(err => res.status(422).json(err));
+    // },
     update: function (req, res) {
-        db.ToDo
+        db
             .findOneAndUpdate({ _id: req.params.id }, req.body)
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
-    },
-    remove: function (req, res) {
-        db.ToDo
-            .findById({ _id: req.params.id })
-            .then(dbModel => dbModel.remove())
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err));
     }
+    // remove: function (req, res) {
+    //     db
+    //         .findById(req.params.id)
+    //         .then(dbModel => dbModel.remove())
+    //         .then(dbModel => res.json(dbModel))
+    //         .catch(err => res.status(422).json(err));
+    // }
 };
