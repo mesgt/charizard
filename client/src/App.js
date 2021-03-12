57718019ce9457d48636c5fc217ad9ac0c8bc159
JMan <justinvang07@gmail.com>
2021-03-11 18:00:41 -0600

    made more changes to login css

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <Main />
+      {/* <Main /> */}
       <Login />
     </div>
   );
--- client/src/components/LoginForm/login.js
+++ client/src/components/LoginForm/login.js
@@ -2,9 +2,6 @@ import React from "react";
 import { GoogleLogin } from 'react-google-login';
 import "./login.css";
 
-
-
-
 function LoginForm() {
 
     const responseGoogle = (response) => {
@@ -18,16 +15,16 @@ function LoginForm() {
             <div class="cell small-4" />
             <div class="cell small-4 logincontainer">
                 <div class="log-in-form">
-                    <h4 class="text-center">Log in to Everything</h4>
+                    <h4 class="text-center">Signup with Everything</h4>
                     <input type="text" class="log-in-form-firstname" name="firstName" placeholder="First Name" />
                     <input type="text" class="log-in-form-lastname" name="lastName" placeholder="Last Name" />
                     <input type="email" class="log-in-form-email" name="email" placeholder="Email" />
                     <input type="password" class="log-in-form-password" name="password" placeholder="Password" />
-                    <input type="submit" class="button expanded log-in-form-button" value="Log in" />
+                    <input type="submit" class="button log-in-form-button" value="Sign Up" />
                     <div class="googleBtn">
                         <GoogleLogin
                             clientId="49214406530-t4ofc8gge6vgfdchf8k6v3e28b883er9.apps.googleusercontent.com"
-                            buttonText="Login with Google"
+                            buttonText="Sign Up with Google"
                             onSuccess={responseGoogle}
                             isSignedIn={false}
                             onFailure={responseGoogle}
@@ -37,6 +34,14 @@ function LoginForm() {
                     </div>
                 </div>
             </div>
+            {/* <div class="cell small-4 signupcontainer">
+                <div class="sign-up-form">
+                    <div class="sign-up-stuff">
+                    <h4 class="text-center">Sign Up</h4>
+                    <input type="submit" class="button expanded log-in-form-button" value="Sign Up" />
+                    </div>
+                </div>
+            </div> */}
         </div>
         
       </>
