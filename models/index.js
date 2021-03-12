e5b44f1b3e2b8771d8d5bd3c1c17ddc6a250e67e
Maria Sargent <maria.marshall.83@gmail.com>
2021-03-12 10:45:17 -0600

    Added note schema.

--- models/index.js
+++ models/index.js
@@ -3,39 +3,8 @@ const Schema = mongoose.Schema;
 
 
 const usersSchema = new Schema({
- //login schema:
-      email: { 
-        type: String,
-        unique: true,
-        match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
-        required: true 
-      },
-      firstName: { 
-        type: String,
-        trim: true, 
-        required: true 
-      },
-      lastName: { 
-        type: String,
-        trim: true, 
-        required: true 
-      },
-      password: {
-        type: String,
-        trim: true,
-        required: "Password is Required", //on submit check is password exists and if not, require password
-        validate: [({ length }) => length >= 6, "Password should be longer."]
-      }, 
+ 
 
-      // note schema
-      note: [{
-        title: { 
-          type: String, 
-          required: true },
-        body: { 
-          type: String, 
-          required: true },
-      }],
       todo: [
         {
           title: { 
---
+++ models/notesSchema.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const noteSchema = new Schema({
+    note: [{
+        title: {
+            type: String,
+            required: true
+        },
+        body: {
+            type: String,
+            required: true
+        },
+    }]
+})
+
+const Note = mongoose.model("Note", noteSchema);
+
+module.exports = Note;
