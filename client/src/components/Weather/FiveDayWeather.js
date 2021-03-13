f978dae29043c08f4c60442f1250d76749f2ced1
Chad Clark <chadclark12@hotmail.com>
2021-03-13 01:06:41 -0600

    working modal

--- client/src/components/Weather/FiveDayWeather.js
+++ client/src/components/Weather/FiveDayWeather.js
@@ -2,35 +2,38 @@ import React from "react";
 import Modal from "react-modal";
 
 const FiveDayWeather = ({ weather, open, onClose, onRequestClose }) => {
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      backgroundColor: "#fff",
+    },
+    overlay: {
+      backgroundColor: "gray",
+    },
+  };
   console.log(weather.daily?.[0]);
   return (
-    <Modal
-      isOpen={open}
-      onRequestClose={onRequestClose}
-      style={{
-        overlay: {
-          backgroundColor: "rgba(255, 255, 255, 0.75)",
-        },
-        content: {
-          backgroundColor: "rgba(255, 255, 255, 1)",
-        },
-      }}
-    >
-      {/* <div className="flex-container">
+    <Modal isOpen={open} onRequestClose={onRequestClose} style={customStyles}>
+      <div className="flex-container">
         <div class="grid-x grid-margin-x small-up-5 ">
           <div className="cell">
             <div className="card">
               <div className="card-section">
                 <img
-                  src={`https://openweathermap.org/img/wn/${weather.daily[0].weather[0].icon}@2x.png`}
+                  src={`https://openweathermap.org/img/wn/${weather.daily?.[0].weather[0].icon}@2x.png`}
                   alt={""}
                 />
               </div>
               <div className="card-section">
-                <p>{weather.daily[0].weather[0].description}</p>
-                <p>Low {weather.daily[0].temp.min.toFixed(0)}°</p>
-                <p>High {weather.daily[0].temp.max.toFixed(0)}°</p>
-                <p>Humidity {weather.daily[0].humidity.toFixed(0)}%</p>
+                <p>{weather.daily?.[0].weather[0].description}</p>
+                <p>Low {weather.daily?.[0].temp.min.toFixed(0)}°</p>
+                <p>High {weather.daily?.[0].temp.max.toFixed(0)}°</p>
+                <p>Humidity {weather.daily?.[0].humidity.toFixed(0)}%</p>
               </div>
             </div>
           </div>
@@ -38,15 +41,15 @@ const FiveDayWeather = ({ weather, open, onClose, onRequestClose }) => {
             <div className="card">
               <div className="card-section">
                 <img
-                  src={`https://openweathermap.org/img/wn/${weather.daily[1].weather[0].icon}@2x.png`}
+                  src={`https://openweathermap.org/img/wn/${weather.daily?.[1].weather[0].icon}@2x.png`}
                   alt={""}
                 />
               </div>
               <div className="card-section">
-                <p>{weather.daily[0].weather[0].description}</p>
-                <p>Low {weather.daily[0].temp.min.toFixed(0)}°</p>
-                <p>High {weather.daily[0].temp.max.toFixed(0)}°</p>
-                <p>Humidity {weather.daily[0].humidity.toFixed(0)}%</p>
+                <p>{weather.daily?.[0].weather[0].description}</p>
+                <p>Low {weather.daily?.[0].temp.min.toFixed(0)}°</p>
+                <p>High {weather.daily?.[0].temp.max.toFixed(0)}°</p>
+                <p>Humidity {weather.daily?.[0].humidity.toFixed(0)}%</p>
               </div>
             </div>
           </div>
@@ -54,15 +57,15 @@ const FiveDayWeather = ({ weather, open, onClose, onRequestClose }) => {
             <div className="card">
               <div className="card-section">
                 <img
-                  src={`https://openweathermap.org/img/wn/${weather.daily[2].weather[0].icon}@2x.png`}
+                  src={`https://openweathermap.org/img/wn/${weather.daily?.[2].weather[0].icon}@2x.png`}
                   alt={""}
                 />
               </div>
               <div className="card-section">
-                <p>{weather.daily[0].weather[0].description}</p>
-                <p>Low {weather.daily[0].temp.min.toFixed(0)}°</p>
-                <p>High {weather.daily[0].temp.max.toFixed(0)}°</p>
-                <p>Humidity {weather.daily[0].humidity.toFixed(0)}%</p>
+                <p>{weather.daily?.[0].weather[0].description}</p>
+                <p>Low {weather.daily?.[0].temp.min.toFixed(0)}°</p>
+                <p>High {weather.daily?.[0].temp.max.toFixed(0)}°</p>
+                <p>Humidity {weather.daily?.[0].humidity.toFixed(0)}%</p>
               </div>
             </div>
           </div>
@@ -70,15 +73,15 @@ const FiveDayWeather = ({ weather, open, onClose, onRequestClose }) => {
             <div className="card">
               <div className="card-section">
                 <img
-                  src={`https://openweathermap.org/img/wn/${weather.daily[3].weather[0].icon}@2x.png`}
+                  src={`https://openweathermap.org/img/wn/${weather.daily?.[3].weather[0].icon}@2x.png`}
                   alt={""}
                 />
               </div>
               <div className="card-section">
-                <p>{weather.daily[0].weather[0].description}</p>
-                <p>Low {weather.daily[0].temp.min.toFixed(0)}°</p>
-                <p>High {weather.daily[0].temp.max.toFixed(0)}°</p>
-                <p>Humidity {weather.daily[0].humidity.toFixed(0)}%</p>
+                <p>{weather.daily?.[0].weather[0].description}</p>
+                <p>Low {weather.daily?.[0].temp.min.toFixed(0)}°</p>
+                <p>High {weather.daily?.[0].temp.max.toFixed(0)}°</p>
+                <p>Humidity {weather.daily?.[0].humidity.toFixed(0)}%</p>
               </div>
             </div>
           </div>
@@ -86,20 +89,20 @@ const FiveDayWeather = ({ weather, open, onClose, onRequestClose }) => {
             <div className="card">
               <div className="card-section">
                 <img
-                  src={`https://openweathermap.org/img/wn/${weather.daily[4].weather[0].icon}@2x.png`}
+                  src={`https://openweathermap.org/img/wn/${weather.daily?.[4].weather[0].icon}@2x.png`}
                   alt={""}
                 />
               </div>
               <div className="card-section">
-                <p>{weather.daily[0].weather[0].description}</p>
-                <p>Low {weather.daily[0].temp.min.toFixed(0)}°</p>
-                <p>High {weather.daily[0].temp.max.toFixed(0)}°</p>
-                <p>Humidity {weather.daily[0].humidity.toFixed(0)}%</p>
+                <p>{weather.daily?.[0].weather[0].description}</p>
+                <p>Low {weather.daily?.[0].temp.min.toFixed(0)}°</p>
+                <p>High {weather.daily?.[0].temp.max.toFixed(0)}°</p>
+                <p>Humidity {weather.daily?.[0].humidity.toFixed(0)}%</p>
               </div>
             </div>
           </div>
         </div>
-      </div> */}
+      </div>
       <a onClick={onClose} class="button primary" href="#/">
         Close
       </a>
