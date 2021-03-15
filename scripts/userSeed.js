027faf3d96baafb7e6dcc8ccf55d32ad3ef1fbe5
Maria Sargent <maria.marshall.83@gmail.com>
2021-03-15 10:38:12 -0500

    Added catch seed connection.

--- scripts/userSeed.js
+++ scripts/userSeed.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 const db = require("../models");
 
 mongoose.connect(
-    process.env.MONGODB_URI ||
+    process.env.MONGODB_URI_SEED ||
     "mongodb://localhost/everythingApp"
-);
+)
+.catch(err => console.log(err));;
 
 const userSeed = [
     {
