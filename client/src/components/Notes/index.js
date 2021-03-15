693ebd8763953e35cf09d30d029c5a717236e043
JMan <justinvang07@gmail.com>
2021-03-15 11:37:04 -0500

    displaying notes on dashboard

--- client/src/components/Notes/index.js
+++ client/src/components/Notes/index.js
@@ -5,10 +5,27 @@ function Notes() {
     return (
         <div className="grid-container fluid NoteBox">
             <div className="grid-x grid-margin-x">
-                <div className="cell small-12"> <span className= "NoteHeader">Note Taker</span></div>
-            </div>
-            <div className="grid-x grid-margin-x">
-                <div className="cell small-12"></div>
+                <nav class="navbar navbar-dark bg-dark col-12">
+                    <a class="navbar-brand">Note Taker</a>
+                    <div class="icons">
+                    <i class="fas fa-save text-light save-note"></i>
+                    <i class="fas fa-pen text-light new-note"></i>
+                    </div>
+                </nav>
+                <div class="container-fluid">
+                    <div class="row">
+                    <div class="col-4 list-container">
+                        <div class="card">
+                        <ul class="list-group">
+                        </ul>
+                        </div>
+                    </div>
+                    <div class="col-8">
+                        <input class="note-title" placeholder="Note Title" maxlength="28" type="text" />
+                        <textarea class="note-textarea" placeholder="Note Text"></textarea>
+                    </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
