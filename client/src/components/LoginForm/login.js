d116e5ad539359237ef3142524a887f4dfed8932
ymasog <yer.masog@gmail.com>
2021-03-15 12:23:49 -0500

    deleted extra login button

--- client/src/components/LoginForm/login.js
+++ client/src/components/LoginForm/login.js
@@ -29,7 +29,6 @@ function LoginForm() {
                         <h4 class="text-center loginHeading">Login with Everything</h4>
                         <input type="email" class="log-in-form-email" name="email" placeholder="Email" />
                         <input type="password" class="log-in-form-password" name="password" placeholder="Password" />
-                        <input type="submit" class="button log-in-form-button" value="Login" />
                         <div class="googleBtn">
                             <GoogleLogin
                                 clientId="49214406530-t4ofc8gge6vgfdchf8k6v3e28b883er9.apps.googleusercontent.com"
@@ -44,16 +43,7 @@ function LoginForm() {
                     </div>
                 </div>
             </div>
-            <GoogleLogin
-                clientId="49214406530-t4ofc8gge6vgfdchf8k6v3e28b883er9.apps.googleusercontent.com"
-                buttonText="Login with Google"
-                onSuccess={redirect}
-                isSignedIn={false}
-                onFailure={failedLogin}
-                cookiePolicy={'single_host_origin'}
-                scope="https://www.googleapis.com/auth/calendar.events"
-                
-            />
+         
         </>
     )
 }
