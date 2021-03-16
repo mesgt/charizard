03a1d3d57c3b1d9e6396785d91bd61315c5ed749
Chad Clark <chadclark12@hotmail.com>
2021-03-16 14:21:31 -0500

    new main merge

--- client/src/components/Calendar/index.js
+++ client/src/components/Calendar/index.js
@@ -30,16 +30,49 @@ const localizer = momentLocalizer(moment);
 //   }
 // ]
 
-const MyCalendar = (props) => (
-  <div className="padding-3">
-    <Calendar
-      localizer={localizer}
-      events={[]}
-      startAccessor="start"
-      endAccessor="end"
-      style={{ height: 400 }}
-    />
-  </div>
-);
+const MyCalendar = (props) => {
+  const [events, setEvents] = React.useState([]);
+  const onSelectEvent = (pEvent) => {
+    const confirm = window.confirm(
+      "Are You Sure You Would Like To Remove Event?"
+    );
+    if (confirm) {
+      console.log(pEvent);
+      const allEvents = [...events];
+      console.log(allEvents);
+      const newEvents = allEvents.filter((title) => title != pEvent.title);
+      console.log(newEvents);
+    }
+  };
+  const handleSelect = ({ start, end }) => {
+    // console.log(start, end);
+    const title = window.prompt("New Event name");
+    if (title)
+      setEvents([
+        ...events,
+        {
+          start,
+          end,
+          title,
+        },
+      ]);
+  };
+  return (
+    <div className="padding-3">
+      <Calendar
+        popup
+        selectable
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 550, width: 800 }}
+        onSelectEvent={(event) => alert(event.title)}
+        onSelectEvent={(event) => onSelectEvent(event)}
+        onSelectSlot={handleSelect}
+      />
+    </div>
+  );
+};
 
 export default MyCalendar;
