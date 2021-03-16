5f259cde63eba7642c8374e94cd46d4a06d8e5ad
JMan <justinvang07@gmail.com>
2021-03-16 11:25:53 -0500

    working notes app!!!!!

--- client/src/components/NotesBody/notesBody.js
+++ client/src/components/NotesBody/notesBody.js
@@ -30,12 +30,12 @@ const NotesBody = ({ activeNote, onUpdateNote }) => {
                 onChange={(e) => onEditField("body", e.target.value)}
             />
             </div>
-            <div className="app-main-note-preview">
+            {/* <div className="app-main-note-preview">
             <h1 className="preview-title">{activeNote.title}</h1>
             <ReactMarkdown className="markdown-preview">
                 {activeNote.body}
             </ReactMarkdown>
-            </div>
+            </div> */}
         </div>
     );
 };
