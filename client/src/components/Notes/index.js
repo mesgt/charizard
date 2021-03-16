0ed98fd3a8a8edbff4c2ec73ee905bcb8e8795ba
JMan <justinvang07@gmail.com>
2021-03-16 10:47:05 -0500

    updated notes app with logic

--- client/src/components/Notes/index.js
+++ client/src/components/Notes/index.js
@@ -24,32 +24,40 @@ function Notes() {
 
         setNotes([newNote, ...notes]);
         setActiveNote(newNote.id);
-    }
+    };
+
+    const onDeleteNote = (noteId) => {
+        setNotes(notes.filter(({ id }) => id !== noteId));
+    };
+
+    const onUpdateNote = (updatedNote) => {
+        const updatedNotesArr = notes.map((note) => {
+            if (note.id === updatedNote.id) {
+                return updatedNote;
+            }
+
+            return note;
+        });
+
+        setNotes(updatedNotesArr);
+    };
+
+    const getActiveNote = () => {
+        return notes.find(({ id }) => id === activeNote);
+    };
     
 
     return (
         <div className="grid-container fluid NoteBox">
             <div className="grid-x grid-margin-x">
-                <nav class="navbar navbar-dark bg-dark col-12">
-                    <a class="navbar-brand">Note Taker</a>
-                    <SaveBtn  />
-                    <NewNoteBtn onClick={() => newNote()} />
-                </nav>
-                <div class="container-fluid">
-                    <div class="row">
-                    <div class="col-4 list-container">
-                        <div class="card">
-                        <ul class="list-group">
-                            <DeleteNoteBtn />
-                        </ul>
-                        </div>
-                    </div>
-                    <div class="col-8">
-                        <input class="note-title" ref={inputTitleRef} placeholder="Note Title" maxlength="28" type="text" />
-                        <textarea class="note-textarea" ref={inputTextRef} placeholder="Note Text"></textarea>
-                    </div>
-                    </div>
-                </div>
+                <Sidebar
+                    notes={notes}
+                    onAddNote={onAddNote}
+                    onDeleteNote={onDeleteNote}
+                    activeNote={activeNote}
+                    setActiveNote={setActiveNote}
+                />
+                <NotesBody activeNote={getActiveNote()} onUpdateNote={onUpdateNote} />
             </div>
         </div>
     )
