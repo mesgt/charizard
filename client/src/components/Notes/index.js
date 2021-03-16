83641e0777e4cd3618fd00eb425e617bef144741
JMan <justinvang07@gmail.com>
2021-03-16 10:56:24 -0500

    updated notes body with logic

--- client/src/components/Notes/index.js
+++ client/src/components/Notes/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import uuid from "react-uuid";
 import "./notes.css";
 import Sidebar from "../NotesSidebar/sidebar";
+import NotesBody from "../NotesBody/notesBody";
 import API from "../../utils/API";
 
 
