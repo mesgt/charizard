963da49956f3bf756858b8cb2ad427ebcdd805fe
JMan <justinvang07@gmail.com>
2021-03-16 10:38:24 -0500

    updating logic for notes app

--- client/src/components/Notes/index.js
+++ client/src/components/Notes/index.js
@@ -1,55 +1,31 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
+import uuid from "react-uuid";
 import "./notes.css";
 import API from "../../utils/API";
-import SaveBtn from "../SaveBtn/savebtn";
-import NewNoteBtn from "../NewNoteBtn/newNoteBtn";
-import DeleteNoteBtn from "../DeleteNoteBtn/deleteNoteBtn";
+
 
 function Notes() {
-    const [notes, setNotes] = useState([])
+    const [notes, setNotes] = useState(
+        localStorage.notes ? JSON.parse(localStorage.notes) : []
+    );
+    const [activeNote, setActiveNote] = useState(false);
 
-    const [note, setNote] = useState([])
-
-    const [saveBtnState, setSaveBtnState] = useState(false)
-
-    const inputTitleRef = useRef();
-    const inputTextRef = useRef();
-
-    //Load all Note tasks and store them with setToDos
     useEffect(() => {
-        loadNotes()
-    }, [])
+        localStorage.setItem("notes", JSON.stringify(notes));
+    }, [notes]);
 
-    //Load and display all Note tasks
-    function loadNotes() {
-        API.getNotes()
+    const onAddNote = () => {
+        const newNote = {
+            id: uuid(),
+            title: "Untitled Note",
+            body: "",
+            lastModified: Date.now(),
+        };
 
-            .then(res => setNotes(res.data))
-            .catch(error => console.log(error));
-
-    };
-
-    //Delete Note task using _id and reload list
-    function deleteNote(id) {
-        API.deleteNote(id)
-
-            .then(res => console.log(res), loadNotes())
-            .catch(error => console.log(error));
-
-    };
-
-    //Edit Note task using _id and reload list
-    function saveNote() {
-        API.saveNote({
-            title: inputTitleRef.current.value,
-            body: inputTextRef.current.value
-        })
-            .then(res => loadNotes())
-            .catch(error => console.log(error));
+        setNotes([newNote, ...notes]);
+        setActiveNote(newNote.id);
     }
-    function newNote() {
-        window.location.reload(); //refresh our title and body of Note
-    }
+    
 
     return (
         <div className="grid-container fluid NoteBox">
